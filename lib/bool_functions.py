8c95daab7bdf5a1eac53459f4f916010b0b1324b
kvn <kvnareba@gmail.com>
2024-06-14 05:29:25 +0300

    comp

--- lib/bool_functions.py
+++ lib/bool_functions.py
@@ -1,2 +1,7 @@
-def return_true():
-    return False
+#!/usr/bin/env python3
+
+from bool_functions import return_true
+
+def test_return_true():
+    '''in bool_functions, function "return_true" returns True.'''
+    assert return_true() == False
